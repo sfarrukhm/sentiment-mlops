3aebd39b79f61df996a66866bb4752f902b34638
Ubuntu <ubuntu@ip-172-31-44-47.us-west-2.compute.internal>
2025-10-05 18:57:50 +0000

    server is logging successfully

--- Makefile
+++ Makefile
@@ -1,16 +1,8 @@
-# Use flake8 or pylint or ruff — here we’ll use ruff (fast modern linter)
-# You can switch to flake8 or pylint if you prefer.
+# Lint all Python files recursively with pylint
 
-# The default target when you just run `make`
 .PHONY: lint
 lint:
-	@echo "🔍 Linting all Python files..."
-	@find . -type f -name "*.py" | xargs ruff check
+	@echo "🔍 Linting all Python files with pylint..."
+	@find . -type f -name "*.py" | xargs pylint --disable=R,C
 	@echo "✅ Linting complete!"
 
-# Optional: autofix formatting issues
-.PHONY: lint-fix
-lint-fix:
-	@echo "🧹 Auto-fixing lint issues..."
-	@find . -type f -name "*.py" | xargs ruff check --fix
-	@echo "✨ Auto-fix complete!"
--- app/serve.py
+++ app/serve.py
@@ -1,28 +1,46 @@
-import os
 from fastapi import FastAPI, Request
-from transformers import AutoTokenizer, AutoModelForSequenceClassification
-import torch
 from app.utils import load_model, predict_text
 
 
 import time
 import logging
-logging.basicConfig(filename='app.log',
-                    level=logging.INFO,
-                    format='%(asctime)s | %(levelname)s | %(message)s'
-                    )
+import os
+
+
+# create file handler
+LOG_DIR = os.path.join(os.path.dirname(__file__), "../logs")
+os.makedirs(LOG_DIR, exist_ok=True)
+file_handler = logging.FileHandler(os.path.join(LOG_DIR, "app.log"))
+
+formatter = logging.Formatter("%(asctime)s | %(levelname)s | %(message)s")
+file_handler.setFormatter(formatter)
+
+# get root logger and attach
+logger=logging.getLogger()
+logger.setLevel(logging.INFO)
+logger.addHandler(file_handler)
+
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(formatter)
+logger.addHandler(console_handler)
 
 # --- Initialize app and model ---
 app = FastAPI(title="Sentiment Inference API")
 model, tokenizer = load_model()
+
+# ---- Check connectivity
+@app.get("/ping")
+def ping():
+    return {"status":200}
+
 @app.get("/predict")
-async def predict(request: Request, text:str)
+async def predict(request: Request, text:str):
     """API endpoint for sentiment prediction."""
-    strat_time=time.time()
+    start_time=time.time()
     result=predict_text(model, tokenizer, text)
     latency=(time.time()-start_time)*1000 # ms
 
     client_ip = request.client.host
-    logging.info(
+    logger.info(
     f"Client: {client_ip} | Text Length: {len(text)} | Latency: {latency: .2f} ms | Result: {result}")
     return {"sentiment":result, "latency_ms":latency}
